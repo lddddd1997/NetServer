669da0cb220f0a007dee3c44eec1500dc6291403
p520c <lddddd1997@gmail.com>
2021-07-23 16:23:46 +0800

    Add Tcp connection

--- TcpConnection.cpp
+++ TcpConnection.cpp
@@ -3,3 +3,45 @@
 * @brief    tcp connection
 * @author   lddddd (https://github.com/lddddd1997)
 */
+
+#include <TcpConnection.h>
+
+TcpConnection::TcpConnection(EventLoop *loop, int fd_,
+                  const struct sockaddr_in& local_addr, const struct sockaddr_in& peer_addr) :
+    
+    local_addr_(local_addr),
+    peer_addr_(peer_addr)
+    
+{
+    
+}
+
+TcpConnection::~TcpConnection()
+{
+
+}
+
+void TcpConnection::CommitChannelToLoop()
+{
+
+}
+
+void TcpConnection::HandleRead()
+{
+
+}
+
+void TcpConnection::HandleWrite()
+{
+
+}
+
+void TcpConnection::HandleClose()
+{
+
+}
+
+void TcpConnection::HandleError()
+{
+
+}
--- TcpConnection.h
+++ TcpConnection.h
@@ -10,25 +10,38 @@
 #include <memory>
 #include <Channel.h>
 #include <EventLoop.h>
+#include <arpa/inet.h>
 
 class TcpConnection : public std::enable_shared_from_this<TcpConnection>
 {
 public:
     using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
-    using Callback = std::function<void (const TcpConnectionPtr&)>
+    using Callback = std::function<void (const TcpConnectionPtr&)>;
 
     TcpConnection(EventLoop *loop, int fd_,
-                  const struct sockaddr_in& local_addr, const struct sockaddr_in& Peer_addr);
+                  const struct sockaddr_in& local_addr, const struct sockaddr_in& peer_addr);
     ~TcpConnection();
-    int Fd() const { return fd_; }
-    EventLoop *GetLoop() const { return loop_; }
+    
+    int Fd() const
+    {
+        return fd_;
+    }
+
+    EventLoop *GetLoop() const
+    {
+        return loop_;
+    }
+
     void CommitChannelToLoop();
+
 private:
     int fd_;
     EventLoop *loop_;
     std::unique_ptr<Channel> channel_;
-    const struct sockaddr_in local_addr_;
-    const struct sockaddr_in Peer_addr_;
+    // const struct sockaddr_in local_addr_;
+    // const struct sockaddr_in Peer_addr_;
+    struct sockaddr_in local_addr_;
+    struct sockaddr_in peer_addr_;
     std::string buffer_in_;
     std::string buffer_out_; 
     
