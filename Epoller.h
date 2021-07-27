67c995e7e627550cce67b082c8e083703a3a3d35
p520c <lddddd1997@gmail.com>
2021-07-27 22:35:07 +0800

    Modify some notes

--- Epoller.h
+++ Epoller.h
@@ -21,14 +21,14 @@ public:
     Epoller();
     ~Epoller();
 
-    void EpollWait(int timeout_ms, ChannelPtrList &active_channel_list); // epollwait封装，超时时间timeout_ms
+    void EpollWait(int timeout_ms, ChannelPtrList &active_channel_list); // epoll_wait封装，超时时间timeout_ms
     void CommitChannelToEpoller(Channel *channel); // EPOLL_CTL_ADD，提交channel事件到epoll树中
     void RemoveChannelFromEpoller(Channel *channel); // EPOLL_CTL_DEL，从epoll树中移除channel事件
     void UpdateChannelInEpoller(Channel *channel); // EPOLL_CTL_MOD，更新epoll树中的channel事件
 
 private:
     int epollfd_; // epoll文件描述符
-    EventList event_list_; // events数组，epollwait的传出参数
+    EventList event_list_; // events数组，epoll_wait的传出参数
     ChannelMap channel_map_; // 事件集合
     static const int EVENTLISTSIZE = 16; // 初始events数组大小
     void FilleActiveChannels(int nfds, ChannelPtrList &active_channel_list); // 填充活跃事件
