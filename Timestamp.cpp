5014affe3916f5410fb7b7c407199979f1b4a3a5
ubuntu <lddddd1997@gmail.com>
2021-08-01 23:59:00 +0800

    Add timestamp

---
+++ Timestamp.cpp
@@ -0,0 +1,6 @@
+/**
+* @file     Timestamp.cpp
+* @brief    timestamp
+* @author   lddddd (https://github.com/lddddd1997)
+*/
+#include <Timestamp.h>
---
+++ Timestamp.h
@@ -0,0 +1,19 @@
+/**
+* @file     Timestamp.h
+* @brief    timestamp
+* @author   lddddd (https://github.com/lddddd1997)
+*/
+#ifndef TIMESTAMP_H_
+#define TIMESTAMP_H_
+
+class Timestamp
+{
+public:
+    Timestamp();
+    ~Timestamp();
+    
+private:
+
+};
+
+#endif
