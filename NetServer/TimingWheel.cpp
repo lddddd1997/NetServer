c03949900d8a0397491c88a9234aff177eeb3320
ubuntu <lddddd1997@gmail.com>
2021-08-03 22:42:04 +0800

    Add TimingWheel

---
+++ NetServer/TimingWheel.cpp
@@ -0,0 +1,69 @@
+/**
+* @file     TimingWheel.h
+* @brief    时间轮，剔除空闲连接
+* @author   lddddd (https://github.com/lddddd1997)
+*/
+#include <boost/any.hpp>
+#include <iostream>
+#include "TimingWheel.h"
+
+ConnectionOnWheel::ConnectionOnWheel(const TcpConnectionWPtr& weak_conn) :
+    weak_connection_(weak_conn)
+{
+
+}
+
+ConnectionOnWheel::~ConnectionOnWheel() // 时间轮上的Tcp连接的引用计数为0，表示连接空闲到期
+{
+    TcpConnectionSPtr connection = weak_connection_.lock();
+    if(connection != nullptr)
+    {
+        connection->Shutdown();
+    }
+}
+
+TimingWheel::TimingWheel(int idle_seconds) :
+    connection_buckets_(idle_seconds)
+{
+    connection_buckets_.resize(idle_seconds);
+}
+
+TimingWheel::~TimingWheel()
+{
+
+}
+
+void TimingWheel::Start()
+{
+    std::cout << "Timing wheel start..." << std::endl;
+}
+
+void TimingWheel::CommitNewConnection(const TcpConnectionSPtr& sp_tcp_connection) // 提交新连接到时间轮
+{
+    if(sp_tcp_connection->Connected())
+    {
+        ConnectionOnWheelSPtr sp_connection_on_wheel(new ConnectionOnWheel(sp_tcp_connection));
+        connection_buckets_.back().insert(sp_connection_on_wheel);
+        ConnectionOnWheelWPtr wp_connection_on_wheel(sp_connection_on_wheel);
+        sp_tcp_connection->SetContext(wp_connection_on_wheel);
+    }
+    else
+    {
+        std::cout << "Connection not established" << std::endl;
+    }
+}
+
+void TimingWheel::Update(const TcpConnectionSPtr& sp_tcp_connection) // 更新连接
+{
+    ConnectionOnWheelWPtr wp_connection_on_wheel(boost::any_cast<ConnectionOnWheelWPtr>(sp_tcp_connection->Context()));
+    ConnectionOnWheelSPtr sp_connection_on_wheel(wp_connection_on_wheel.lock());
+    if(sp_connection_on_wheel != nullptr)
+    {
+        connection_buckets_.back().insert(sp_connection_on_wheel);
+    }
+}
+
+void TimingWheel::TimeLapse()
+{
+    connection_buckets_.push_back(Bucket());
+}
---
+++ NetServer/TimingWheel.h
@@ -0,0 +1,49 @@
+/**
+* @file     TimingWheel.cpp
+* @brief    时间轮，剔除空闲连接
+* @author   lddddd (https://github.com/lddddd1997)
+*/
+#ifndef CONNECTION_ON_WHEEL_H_
+#define CONNECTION_ON_WHEEL_H_
+
+#include <memory>
+#include <boost/circular_buffer.hpp>
+#include <unordered_set>
+#include "TcpConnection.h"
+
+class ConnectionOnWheel // 时间轮上的Tcp连接
+{
+public:
+    using TcpConnectionWPtr = std::weak_ptr<TcpConnection>;
+    using TcpConnectionSPtr = std::shared_ptr<TcpConnection>;
+
+    explicit ConnectionOnWheel(const TcpConnectionWPtr& weak_conn);
+    ~ConnectionOnWheel();
+
+private:
+    TcpConnectionWPtr weak_connection_;
+};
+
+// https://www.cnblogs.com/Solstice/archive/2011/05/04/2036983.html
+class TimingWheel // 时间轮
+{
+public:
+    using TcpConnectionSPtr = std::shared_ptr<TcpConnection>;
+    using ConnectionOnWheelSPtr = std::shared_ptr<ConnectionOnWheel>;
+    using ConnectionOnWheelWPtr = std::weak_ptr<ConnectionOnWheel>;
+    using Bucket = std::unordered_set<ConnectionOnWheelSPtr>;
+    using ConnectionBuckets = boost::circular_buffer<Bucket>;
+    
+    TimingWheel() = delete;
+    explicit TimingWheel(int idle_seconds = 10);
+    ~TimingWheel();
+
+    void Start();
+    void CommitNewConnection(const TcpConnectionSPtr& sp_tcp_connection);
+    void Update(const TcpConnectionSPtr& sp_tcp_connection);
+    void TimeLapse();
+private:
+    ConnectionBuckets connection_buckets_;
+};
+
+#endif
