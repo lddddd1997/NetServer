8bb64b46a6b077f364274b3dc73376e2a7198444
ubuntu <lddddd1997@gmail.com>
2021-08-04 23:24:38 +0800

    Add functions

--- NetServer/TimerQueue.cpp
+++ NetServer/TimerQueue.cpp
@@ -3,6 +3,64 @@
 * @brief    timer queue
 * @author   lddddd (https://github.com/lddddd1997)
 */
-#include <TimerQueue.h>
+#include <sys/timerfd.h>
+#include <unistd.h>
+#include <assert.h>
+#include "TimerQueue.h"
 
+TimerQueue::TimerQueue(EventLoop *loop) :
+    loop_(loop),
+    timer_channel_("timer queue")
+{
+    int timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+    if(timerfd == -1)
+    {
+        perror("TimerQueue::TimerQueue");
+        exit(EXIT_FAILURE);
+    }
+    timer_channel_.SetFd(timerfd);
+    timer_channel_.SetEvents(EPOLLIN | EPOLLET);
+    timer_channel_.SetReadHandler(std::bind(&TimerQueue::ExpiredReadHandler, this));
+    loop_->CommitTaskToLoop(std::bind(&EventLoop::CommitChannelToEpoller, loop_, &timer_channel_)); // 将事件通知加入epoller中
+}
 
+TimerQueue::~TimerQueue()
+{
+    close(timer_channel_.Fd());
+}
+
+void TimerQueue::AddTimer(Timestamp when, double interval, TimerCallback callback)
+{
+    TimerSPtr sp_timer = std::make_shared<Timer>(when, interval, callback);
+    loop_->CommitTaskToLoop(std::bind(&TimerQueue::AddTimerInLoop, this, sp_timer));
+}
+
+void TimerQueue::AddTimerInLoop(TimerSPtr sp_timer) // 引用传递不增加use_count，故不能定义为const TimerSPtr& sp_timer
+{
+    bool earliest_changed = InsertToSet(sp_timer);
+}
+
+bool TimerQueue::InsertToSet(TimerSPtr sp_timer)
+{
+    bool earliest_changed = false;
+    Timestamp when = sp_timer->Expiration();
+    TimerSet::iterator it = timer_set_.begin();
+    if(it == timer_set_.end() || when < it->first)
+    {
+        earliest_changed = true;
+    }
+
+    std::pair<TimerSet::iterator, bool> result = timer_set_.insert(TimerPair(when, sp_timer));
+    assert(result.second);
+    return earliest_changed;
+}
+
+TimerQueue::ExpiredList TimerQueue::ExpiredTimer(Timestamp now)
+{
+    return TimerQueue::ExpiredList();
+}
+
+void TimerQueue::ExpiredReadHandler()
+{
+
+}
--- NetServer/TimerQueue.h
+++ NetServer/TimerQueue.h
@@ -6,14 +6,35 @@
 #ifndef TIMER_QUEUE_H_
 #define TIMER_QUEUE_H_
 
+#include <set>
+#include <vector>
+#include <memory>
+#include "EventLoop.h"
+#include "Timer.h"
+
 class TimerQueue
 {
 public:
-    TimerQueue();
+    using TimerCallback = std::function<void()>;
+    using TimerSPtr = std::shared_ptr<Timer>;
+    using TimerPair = std::pair<Timestamp, TimerSPtr>;
+    using TimerSet = std::set<TimerPair>;
+    using ExpiredList = std::vector<TimerPair>;
+
+    explicit TimerQueue(EventLoop *loop);
     ~TimerQueue();
     
+    void AddTimer(Timestamp when, double interval, TimerCallback callback);
 private:
-
+    EventLoop *loop_;
+    Channel timer_channel_;
+    TimerSet timer_set_;
+    
+    void AddTimerInLoop(TimerSPtr sp_timer);
+    bool InsertToSet(TimerSPtr sp_timer);
+    ExpiredList ExpiredTimer(Timestamp now);
+    void ExpiredReadHandler();
+    
 };
 
 #endif
