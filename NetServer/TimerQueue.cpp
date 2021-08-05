bb3087ecaab37e168d3df4d9c93e4a438efc066f
ubuntu <lddddd1997@gmail.com>
2021-08-05 21:27:08 +0800

    Add timer set management

--- NetServer/TimerQueue.cpp
+++ NetServer/TimerQueue.cpp
@@ -1,12 +1,15 @@
 /**
 * @file     TimerQueue.cpp
-* @brief    timer queue
+* @brief    timer集合管理
 * @author   lddddd (https://github.com/lddddd1997)
 */
 #include <sys/timerfd.h>
 #include <unistd.h>
 #include <assert.h>
+#include <cstring>
+#include <iostream>
 #include "TimerQueue.h"
+#include "EventLoop.h"
 
 TimerQueue::TimerQueue(EventLoop *loop) :
     loop_(loop),
@@ -29,18 +32,50 @@ TimerQueue::~TimerQueue()
     close(timer_channel_.Fd());
 }
 
-void TimerQueue::AddTimer(Timestamp when, double interval, TimerCallback callback)
+void TimerQueue::AddTimer(Timestamp when, double interval, const TimerCallback& callback)
 {
     TimerSPtr sp_timer = std::make_shared<Timer>(when, interval, callback);
-    loop_->CommitTaskToLoop(std::bind(&TimerQueue::AddTimerInLoop, this, sp_timer));
+    loop_->CommitTaskToLoop(std::bind(&TimerQueue::AddTimerInLoop, this, sp_timer)); // bind的参数完美转发，会增加sp_timer的引用计数
 }
 
-void TimerQueue::AddTimerInLoop(TimerSPtr sp_timer) // 引用传递不增加use_count，故不能定义为const TimerSPtr& sp_timer
+void TimerQueue::AddTimerInLoop(const TimerSPtr& sp_timer)
 {
-    bool earliest_changed = InsertToSet(sp_timer);
+    bool earliest_changed = InsertTimerToSet(sp_timer);
+    if(earliest_changed) // 如果添加的定时器到期时间比集合中最早的到期时间早
+    {
+        ResetTimerFd(sp_timer->Expiration());
+    }
 }
 
-bool TimerQueue::InsertToSet(TimerSPtr sp_timer)
+struct timespec TimerQueue::HowMuchTimeFromNow(Timestamp when)
+{
+    int64_t micro_seconds = when.MicroSecondsSinceEpoch() - Timestamp::Now().MicroSecondsSinceEpoch();
+    if(micro_seconds < 100)
+    {
+        micro_seconds = 100;
+    }
+    struct timespec ts;
+    bzero(&ts, sizeof(ts));
+    ts.tv_sec = static_cast<time_t>(micro_seconds / Timestamp::MICRO_SECONDS_PER_SECOND);
+    ts.tv_nsec = static_cast<long>((micro_seconds % Timestamp::MICRO_SECONDS_PER_SECOND) * 1000);
+    return ts;
+}
+
+void TimerQueue::ResetTimerFd(Timestamp expiration)
+{
+    struct itimerspec new_val;
+    struct itimerspec old_val;
+    bzero(&new_val, sizeof(new_val));
+    bzero(&old_val, sizeof(old_val));
+    new_val.it_value = HowMuchTimeFromNow(expiration);
+    if(timerfd_settime(timer_channel_.Fd(), 0, &new_val, &old_val) < 0)
+    {
+        perror("TimerQueue::ResetTimerFd");
+        exit(EXIT_FAILURE);
+    }
+}
+
+bool TimerQueue::InsertTimerToSet(const TimerSPtr& sp_timer)
 {
     bool earliest_changed = false;
     Timestamp when = sp_timer->Expiration();
@@ -55,12 +90,52 @@ bool TimerQueue::InsertToSet(TimerSPtr sp_timer)
     return earliest_changed;
 }
 
-TimerQueue::ExpiredList TimerQueue::ExpiredTimer(Timestamp now)
+TimerQueue::ExpiredList TimerQueue::GetExpiredTimers(Timestamp now)
 {
-    return TimerQueue::ExpiredList();
+    ExpiredList expired;
+    TimerSet::iterator expired_end = timer_set_.lower_bound(TimerPair(now, nullptr)); // 找出过期的定时器
+    std::copy(timer_set_.begin(), expired_end, back_inserter(expired)); // 添加到expired
+    timer_set_.erase(timer_set_.begin(), expired_end); // 从set集合清理掉，second引用计数--
+
+    return expired;
+}
+
+void TimerQueue::ResetTimersIfRepeat(const ExpiredList& expired, Timestamp now)
+{
+    for(const TimerPair& it : expired)
+    {
+        if(it.second->Repeat())
+        {
+            assert(it.first == it.second->Expiration());
+            it.second->ResetExpirationIfRepeat(now); // 重置定时器的到期时间
+            InsertTimerToSet(it.second); // 加入定时器集合
+        }
+        // std::cout << "ResetTimersIfRepeat:" << it.second.use_count() << std::endl;
+    }
+    if(!timer_set_.empty())
+    {
+        Timestamp next_expired = timer_set_.begin()->second->Expiration(); // 取出最早到期的定时器
+        if(next_expired.Valid())
+        {
+            ResetTimerFd(next_expired); // 重置timerfd触发为最早到期的时间
+        }
+    }
 }
 
 void TimerQueue::ExpiredReadHandler()
 {
+    Timestamp now(Timestamp::Now());
+    uint64_t howmany;
+    ssize_t n = ::read(timer_channel_.Fd(), &howmany, sizeof(howmany));
+    if(n != sizeof(howmany))
+    {
+        perror("TimerQueue::ExpiredReadHandler");
+    }
 
+    ExpiredList expired = GetExpiredTimers(now);
+    for(const TimerPair& it : expired)
+    {
+        it.second->ExecuteCallback(); // 处理到期任务
+    }
+    ResetTimersIfRepeat(expired, now);
 }
--- NetServer/TimerQueue.h
+++ NetServer/TimerQueue.h
@@ -1,6 +1,6 @@
 /**
 * @file     TimerQueue.h
-* @brief    timer queue
+* @brief    timer集合管理
 * @author   lddddd (https://github.com/lddddd1997)
 */
 #ifndef TIMER_QUEUE_H_
@@ -9,8 +9,10 @@
 #include <set>
 #include <vector>
 #include <memory>
-#include "EventLoop.h"
 #include "Timer.h"
+#include "Channel.h"
+
+class EventLoop;
 
 class TimerQueue
 {
@@ -24,16 +26,19 @@ public:
     explicit TimerQueue(EventLoop *loop);
     ~TimerQueue();
     
-    void AddTimer(Timestamp when, double interval, TimerCallback callback);
+    void AddTimer(Timestamp when, double interval, const TimerCallback& callback); // 添加定时器，（线程安全的，内部通过CommitTaskToLoop添加）
 private:
     EventLoop *loop_;
-    Channel timer_channel_;
-    TimerSet timer_set_;
+    Channel timer_channel_; // 定时channel
+    TimerSet timer_set_; // 定时器集合
     
-    void AddTimerInLoop(TimerSPtr sp_timer);
-    bool InsertToSet(TimerSPtr sp_timer);
-    ExpiredList ExpiredTimer(Timestamp now);
-    void ExpiredReadHandler();
+    void AddTimerInLoop(const TimerSPtr& sp_timer); // AddTimer会在所处loop线程中调用
+    struct timespec HowMuchTimeFromNow(Timestamp when); // 现在到when的时间差
+    void ResetTimerFd(Timestamp expiration); // 重置timerfd的定时触发时间
+    bool InsertTimerToSet(const TimerSPtr& sp_timer); // 将定时器插入到集合中，返回该定时器的到期时间是否早于集合中的最早到期时间
+    ExpiredList GetExpiredTimers(Timestamp now); // 获取已经到期的定时器
+    void ResetTimersIfRepeat(const ExpiredList& expired, Timestamp now); // 重新将重复定时器插入集合中
+    void ExpiredReadHandler(); // 到期定时器事件回调
     
 };
 
