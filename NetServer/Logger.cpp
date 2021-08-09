c45e2a38394c3153d90e76a50c5feba05f601687
ubuntu <lddddd1997@gmail.com>
2021-08-10 01:36:22 +0800

    Use static variables instead of global variables

--- NetServer/Logger.cpp
+++ NetServer/Logger.cpp
@@ -2,76 +2,79 @@
 * @file     Logger.cpp
 * @brief    日志类封装
 * @author   lddddd (https://github.com/lddddd1997)
+* TODO:     优化线程id的格式化
 */
 #include <thread>
 #include <sstream>
 #include <assert.h>
 #include "Logger.h"
 
-Logger::Logger(const char *source_file, int line, LogLevelEnum level) :
-    base_name_(source_file),
-    line_(line),
-    level_(level)
+const char *Logger::LogLevelName[Logger::NUM_LOG_LEVELS] =
+    {
+        "TRACE ",
+        "DEBUG ",
+        "INFO  ",
+        "WARN  ",
+        "ERROR ",
+        "FATAL ",
+    };
+std::unique_ptr<AsyncLogging> Logger::async_logging_ = nullptr;
+Logger::OutputFunc Logger::log_output_ = Logger::DefaultOutput;
+Logger::FlushFunc Logger::log_flush_ = Logger::defaultFlush;
+Logger::LogLevelEnum Logger::log_level_ = Logger::TRACE;
+
+
+Logger::Logger() :
+    base_name_(__FILE__),
+    line_(__LINE__),
+    level_(Logger::INFO),
+    time_(Timestamp::Now())
 {
+    // SetLogLevel(Logger::TRACE);
     FormatFrontPart();
-    stream_ << LogLevelName[level_];
+    stream_ << Logger::LogLevelName[level_];
 }
 
-Logger::Logger(const char *source_file, int line, LogLevelEnum level, const char *func) :
+Logger::Logger(const char *source_file, int line, Logger::LogLevelEnum level) :
     base_name_(source_file),
     line_(line),
     level_(level),
     time_(Timestamp::Now())
 {
     FormatFrontPart();
-    stream_ << LogLevelName[level];
-    stream_ << func << " ";
+    stream_ << Logger::LogLevelName[level_];
 }
 
-Logger::OutputFunc g_output = Logger::DefaultOutput;
-Logger::FlushFunc g_flush = Logger::defaultFlush;
-Logger::LogLevelEnum g_log_level;
+Logger::Logger(const char *source_file, int line, Logger::LogLevelEnum level, const char *func) :
+    base_name_(source_file),
+    line_(line),
+    level_(level),
+    time_(Timestamp::Now())
+{
+    FormatFrontPart();
+    stream_ << Logger::LogLevelName[level];
+    stream_ << func << " ";
+}
 
 Logger::~Logger()
 {
     FormatBackPart();
-    g_output(stream_.FormatBuffer().Data(), stream_.FormatBuffer().Length());
-    if(level_ == FATAL)
+    log_output_(stream_.FormatBuffer().Data(), stream_.FormatBuffer().Length());
+    if(level_ == Logger::FATAL)
     {
-        g_flush();
+        log_flush_();
         abort();
     }
 }
 
-Logger::LogLevelEnum Logger::LogLevel()
-{
-    return g_log_level;
-}
-
-void Logger::SetLogLevel(Logger::LogLevelEnum level)
-{
-    g_log_level = level;
-}
-
 void Logger::SetOutput(OutputFunc out)
 {
-    g_output = out;
+    log_output_ = out;
 }
 
 void Logger::SetFlush(FlushFunc flush)
 {
-    g_flush = flush;
-}
-
-void Logger::DefaultOutput(const char *log_line_, int len)
-{
-    size_t n = fwrite(log_line_, 1, len, stdout); // fwrite线程安全
-    (void)n; // 去除警告
-}
-
-void Logger::defaultFlush()
-{
-    fflush(stdout);
+    log_flush_ = flush;
 }
 
 thread_local time_t t_last_log_time = 0; // thread_local变量，每个线程有一份，与线程中的局部变量相似，但不用在各个方法内传递
@@ -81,7 +84,7 @@ void Logger::FormatFrontPart()
 {
     time_t seconds = static_cast<time_t>(time_.MicroSecondsSinceEpoch() / Timestamp::MICRO_SECONDS_PER_SECOND);
     int micro_seconds = static_cast<int>(time_.MicroSecondsSinceEpoch() % Timestamp::MICRO_SECONDS_PER_SECOND);
-    // if(seconds != t_last_log_time)
+    if(seconds != t_last_log_time) // 一秒内只更新微秒部分
     {
         t_last_log_time = seconds;
         struct tm tm_time;
@@ -93,10 +96,9 @@ void Logger::FormatFrontPart()
         assert(len == 17);
         t_time_buf[17] = '\0';
     }
-    
     stream_ << t_time_buf;
-    stream_ << "." << micro_seconds << " ";
-    std::ostringstream oss; // 可能会影响效率
+    stream_ << "." << micro_seconds << "Z ";
+    std::ostringstream oss; // 特别影响效率，待优化
     oss << std::this_thread::get_id();
     stream_ << oss.str() << " ";
 }
--- NetServer/Logger.h
+++ NetServer/Logger.h
@@ -1,13 +1,12 @@
 /**
-* @file     Logger.h
+* @file     AsyncLogging.h
 * @brief    日志类封装
 * @author   lddddd (https://github.com/lddddd1997)
 */
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
-#include <atomic>
-#include "LogStream.h"
+#include "AsyncLogging.h"
 #include "Timestamp.h"
 
 #define LOG_TRACE \
@@ -23,11 +22,10 @@
 #define LOG_ERROR Logger(__FILE__, __LINE__, Logger::ERROR).Stream()
 #define LOG_FATAL Logger(__FILE__, __LINE__, Logger::FATAL).Stream()
 
-
 class Logger
 {
 public:
-    using OutputFunc = void(*)(const char *log_line_, int len); // 函数指针效率比function高得多
+    using OutputFunc = void(*)(const char *log_line, int len); // 函数指针效率比function高得多
     using FlushFunc = void(*)();
     enum LogLevelEnum
     {
@@ -39,39 +37,86 @@ public:
         FATAL,
         NUM_LOG_LEVELS
     };
+
+    Logger(); // 用于实例化调用设置Logger方式，default或async
     Logger(const char *source_file, int line, LogLevelEnum level);
     Logger(const char *source_file, int line, LogLevelEnum level, const char *func);
     ~Logger();
 
-    static LogLevelEnum LogLevel();
-    static void SetLogLevel(LogLevelEnum level);
-    static void SetOutput(OutputFunc out);
-    static void SetFlush(FlushFunc flush);
-    static void DefaultOutput(const char *log_line_, int len);
-    static void defaultFlush();
+    void EnableAsyncLogging(const std::string& base_name, int roll_size) // 使能异步日志
+    {
+        if(async_logging_ == nullptr)
+        {
+            async_logging_.reset(new AsyncLogging(base_name, roll_size));
+        }
+        else
+        {
+            async_logging_.reset(new AsyncLogging(base_name, roll_size));
+        }
+        async_logging_->Start();
+        SetOutput(AyncOutput);
 
+        stream_ << __func__ << " ";
+        stream_ << "Enable asynclogging";
+    }
+    void DisableAsyncLogging() // 失能异步日志，使用默认日志输出
+    {
+        // async_logging_.reset(nullptr); // 存在线程安全，宁愿错写也不希望发生访问空指针
+        SetOutput(DefaultOutput);
+        // async_logging_->Stop();
+
+        stream_ << __func__ << " ";
+        stream_ << "Disable asynclogging";
+    }
+    void SetLogLevel(LogLevelEnum level) // 设置全局的日志级别
+    {
+        log_level_ = level;
+    }
+    static LogLevelEnum LogLevel() // 返回全局的日志级别
+    {
+        return log_level_;
+    }
     LogStream& Stream()
     {
         return stream_;
     }
 private:
-    const char *LogLevelName[Logger::NUM_LOG_LEVELS] =
-    {
-        "TRACE ",
-        "DEBUG ",
-        "INFO  ",
-        "WARN  ",
-        "ERROR ",
-        "FATAL ",
-    };
-    const char *base_name_;
-    int line_;
-    LogLevelEnum level_;
-    Timestamp time_;
-    LogStream stream_;
+    const char *base_name_; // 前缀名
+    int line_; // 行号
+    LogLevelEnum level_; // 局部的日志级别
+    Timestamp time_; // 局部的时间戳
+    LogStream stream_; // 局部的日志流
+    
+    static const char *LogLevelName[Logger::NUM_LOG_LEVELS];
+    static std::unique_ptr<AsyncLogging> async_logging_; // 全局的异步日志对象，默认为nullptr
+    static OutputFunc log_output_; // 全局的日志输出方式，目前可设置异步EnableAsyncLogging或默认DisableAsyncLogging
+    static FlushFunc log_flush_; // 全局的日志刷新
+    static LogLevelEnum log_level_; // 全局的日志级别，默认为Logger::TRACE，可使用SetLogLevel接口进行设置
 
-    void FormatFrontPart();
-    void FormatBackPart();
+    static void DefaultOutput(const char *log_line, int len) // 默认输出方式
+    {
+        size_t n = fwrite(log_line, 1, len, stdout); // fwrite线程安全
+        (void)n; // 去除警告
+    }
+    static void defaultFlush() // 默认刷新方式
+    {
+        fflush(stdout);
+    }
+    static void AyncOutput(const char *log_line, int len) // 使用异步日志输出方式，存在线程安全，但几乎只设置一次
+    {
+        if(async_logging_ != nullptr)
+        {
+            async_logging_->Append(log_line, len);
+        }
+        else
+        {
+            std::cout << "async_logging_ was null" << std::endl;
+        }
+    }
+    void SetOutput(OutputFunc out); // 设置日志流的输出方式，默认输出到stdout
+    void SetFlush(FlushFunc flush); // 设置日志流的刷新方式，默认刷新stdout
+    void FormatFrontPart(); // 格式化日志数据的前半部分
+    void FormatBackPart(); // 格式化日志数据的后半部分
 };
 
 #endif
