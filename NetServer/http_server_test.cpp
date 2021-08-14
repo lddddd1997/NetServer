91d51635b5af725ed49f3627d83c11b0cff7396f
ubuntu <lddddd1997@gmail.com>
2021-08-14 09:19:55 +0800

    Add try catch

--- NetServer/http_server_test.cpp
+++ NetServer/http_server_test.cpp
@@ -5,12 +5,20 @@ using namespace std;
 
 int main()
 {
-    // Logger().EnableAsyncLogging("http_server", 5 * 1024 * 1024);
     EventLoop basic_loop("basic");
-    HttpServer http_server(&basic_loop, 8888, 2, 2, 5);
+    HttpServer http_server(&basic_loop, 8888, 1, 0, 0);
     http_server.Start();
 
-    basic_loop.Looping();
+    try
+    {
+        basic_loop.Looping();
+    }
+    catch(const std::exception& ex)
+    {
+        std::cerr << "exception caught int BasicLoopThread::RunInThread at: " << std::this_thread::get_id() << std::endl;
+        std::cerr << "reason: " << ex.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
 
